c7477884b317617f7eb5c36c82fc9682272de10e
OliveShey <142577489+OliveShey@users.noreply.github.com>
2024-06-29 00:34:57 +0800

    Add files via upload

---
+++ assets/js/movies_table.js
@@ -0,0 +1,36 @@
+function fetchMovies() {
+    var year = document.getElementById('year-select1').value;
+    var table = document.getElementById('movies-table');
+    table.innerHTML = ''; // 清空旧数据
+
+    // 创建一个新的行元素
+    var row = '<tr><th>ID</th><th>电影名</th><th>总票房</th></tr>';
+    table.innerHTML += row;
+
+    // 假设每个CSV文件存储在服务器上的/data_csv目录中
+    var csvFilePath = 'data/box_office_' + year + '.csv';
+
+    // 使用fetch API获取文件内容
+    fetch(csvFilePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(csvData => {
+            var rows = csvData.split('\n'); // 按行分割数据
+            for (var i = 1; i < rows.length; i++) {
+                var rowData = rows[i].split('\t');
+                var row = '<tr>';
+                for (var j = 0; j < 3; j++) {
+                    row += '<td>' + rowData[j] + '</td>';
+                }
+                row += '</tr>';
+                table.innerHTML += row;
+            }
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
---
+++ assets/js/piecharts.js
@@ -0,0 +1,72 @@
+// 假设这是您存储电影类型和对应指数的文本文件路径
+const filePath = "C:/Users/XQ/IdeaProjects/JavaLab3_ZHJ&XQ/src/main/webapp/data/genres_and_index.csv";
+var year = document.getElementById('year-select1').value;
+// 获取文件内容
+function readFileContent(filePath) {
+    return fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .catch(error => {
+            console.error('Error fetching file content:', error);
+            throw error;
+        });
+}
+
+// 解析文件内容并创建ECharts图表
+function createPieChart(content) {
+    const data = [];
+    var segment = content.split('20\d\d:\n')[year-2000];
+    var row = segment.split("\n");
+    var genre = row.split(":")[0];
+    var index = row.split(":")[1];
+    data.push({ value: index, name: genre });
+
+    // 创建ECharts实例
+    var chartDom = document.getElementById('piecharts');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    option = {
+        legend: {
+            top: 'bottom'
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                mark: { show: true },
+                dataView: { show: true, readOnly: false },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        series: [
+            {
+                name: 'Nightingale Chart',
+                type: 'pie',
+                radius: [50, 250],
+                center: ['50%', '50%'],
+                roseType: 'area',
+                itemStyle: {
+                    borderRadius: 8
+                },
+                data: data
+            }
+        ]
+    };
+
+    option && myChart.setOption(option);
+}
+
+// 读取文件内容并创建图表
+readFileContent(filePath)
+    .then(content => {
+        createPieChart(content);
+    })
+    .catch(error => {
+        console.error('读取文件时出错:', error);
+    });
+
